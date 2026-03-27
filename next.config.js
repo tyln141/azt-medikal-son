cbf8238493623414a0b40efa7bfba15d0b973e99
Taylan <egementakla@gmail.com>
2026-03-27 14:36:12 +0300

    admin subdomain redirect

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'admin.aztmedikal.com.tr',
+          },
+        ],
+        destination: '/tr/admin',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
